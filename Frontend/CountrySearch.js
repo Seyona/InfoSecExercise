956561cb5779d265331a6bb6d79e758124d18fd1
Seyona <dgbaker93@gmail.com>
2020-12-04 01:42:38 -0600

    add CreateCountryDiv and related helper functions

--- Frontend/CountrySearch.js
+++ Frontend/CountrySearch.js
@@ -37,7 +37,76 @@ async function SearchCountry() {
     }
 }
 
+// Creates the div based on the passed country information
+function CreateCountryDiv(country) {
+    // Create the div for the country information
+            var countryDiv = document.createElement('div');
+            countryDiv.id = country.alpha2;
+            countryDiv.name = country.alpha2;
+            countryDiv.style.textAlign = "center";
 
+            // Create the first span for the Country's name and picture
+            var namePicSpan = document.createElement('span');
+            namePicSpan.class = "name-picture";
+            namePicSpan.style.display = "inline-block";
+            namePicSpan.style.textAlign = "left";
+
+            var picLabel = document.createElement('label');
+            picLabel.setAttribute("for", country.alpha3);
+            picLabel.innerHTML = country.full_name;
+            namePicSpan.appendChild(picLabel);
+
+            var breakTag = document.createElement('br');
+            namePicSpan.appendChild(breakTag);
+
+            var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svg.setAttribute('height', '100');
+            svg.setAttribute('width', '200');
+            svg.setAttribute('id', country.alpha3);
+            namePicSpan.appendChild(svg);
+
+            var pic = document.createElementNS('http://www.w3.org/2000/svg','image');
+            pic.setAttribute('height', '100');
+            pic.setAttribute('width', '200');
+            pic.setAttribute('id', country.alpha2 + "image");
+            pic.setAttribute('href', country.flag);
+
+            svg.appendChild(pic);
+            countryDiv.appendChild(namePicSpan);
+
+            // Create the second span for the remaining Country information
+
+            var infoSpan = document.createElement('span');
+            infoSpan.name = "info";
+            infoSpan.style.display = "inline-block";
+            infoSpan.style.textAlign = "left";
+
+            var infoList = document.createElement('ul');
+            infoList.style.margin = "auto";
+            infoList.style.listStyleType = "none";
+
+            createLi(infoList, "alpha2", country.alpha2);
+            createLi(infoList, "alpha3", country.alpha3);
+            createLi(infoList, "population", country.population);
+            createLi(infoList, "region", country.region);
+
+            var languagesCSV = country.languages.join(", "); // convert the array of languages to a CSV for display
+            createLi(infoList, "languages", languagesCSV);
+
+            infoSpan.appendChild(infoList);
+            countryDiv.appendChild(infoSpan);
+
+            // Apply new Div to parent div
+             document.getElementById('country-info').appendChild(countryDiv);
+}
+
+// Creates a li for the passed Unordered List
+function createLi(parentUl, liDescriptor, liData) {
+    var item = document.createElement('li');
+    // set the contents
+    item.appendChild(document.createTextNode(liDescriptor + ": " + liData));
+    parentUl.appendChild(item);
+}
 
 
 /*
